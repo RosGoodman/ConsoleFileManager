269ef06fe68098b96a586fc16d1ff63f6ed50104
Ros <pavel.official@bk.ru>
2021-03-29 19:14:08 +0300

    start dev model and controller

---
+++ ConsoleFileManager/ConsoleFileManager/Controllers/Controller.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleFileManager.Models;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleFileManager.Controls
+{
+    internal class Controller
+    {
+        private FileModel _selectedFile;    //выделенный файл
+
+        /// <summary>Получить информацию о файле.</summary>
+        /// <returns>Информация построчно.</returns>
+        public List<string> GetSelectedFileInfo()
+        {
+            List<string> fileInfo = _selectedFile.GetFileInfo();
+            long size = 0;
+            string createDate = string.Empty;
+
+            if (!_selectedFile.IsFolder)    //если файл не является папкой
+            {
+                var file = new FileInfo(_selectedFile.FilePath);
+                size = file.Length/1024;    //KB
+                createDate = File.GetCreationTime(_selectedFile.FilePath).ToString();
+            }
+
+            fileInfo.Add(size.ToString() + "KB");
+            fileInfo.Add(createDate);
+
+            return fileInfo;
+        }
+    }
+}
---
+++ ConsoleFileManager/ConsoleFileManager/Models/FileListModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleFileManager.Models
+{
+    internal class FileListModel : FileList
+    {
+        private List<FileModel> _fileList;
+
+        public FileListModel(string[] entries)
+        {
+            //TODO: вынести в controller
+            //список всех файлов указанной директории.
+            //string[] entries = Directory.GetFileSystemEntries(workDir, "*", SearchOption.AllDirectories);
+        }
+
+        /// <summary>Получить список файлов.</summary>
+        /// <returns>Список файлов.</returns>
+        public List<FileModel> GetFiles() => _fileList;
+    }
+}
