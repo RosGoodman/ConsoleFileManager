9191d916606b40d233c914f9b628a10322434661
Ros Goodman <pavel.official@bk.ru>
2021-03-30 15:27:56 +0300

    reload with .gitignore

---
+++ ConsoleFileManager/ConsoleFileManager/Models/FileModel.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleFileManager.Models
+{
+    /// <summary>Класс описывающий модель файлов и папок.</summary>
+    internal class FileModel
+    {
+        private string _fileName = string.Empty;
+        private string _filePath = string.Empty;
+        private bool _isFolder = false;
+
+        internal string FilePath => _filePath;  //путь к файлу.
+        internal bool IsFolder => _isFolder;    //является ли файл папкой.
+
+        internal FileModel(string filePath)
+        {
+            _filePath = filePath;   //путь к файлу.
+
+            if (Directory.Exists(filePath)) //папка\не папка.
+                _isFolder = true;
+        }
+
+        /// <summary>Получить данные файла.</summary>
+        /// <returns>Список информации.</returns>
+        internal List<string> GetFileInfo()
+        {
+            List<string> fileInfo = new List<string>();
+            fileInfo.Add(_fileName);
+            fileInfo.Add(_filePath);
+
+            return fileInfo;
+        }
+    }
+}
