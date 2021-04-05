bfb157a02986b307773f2f0d623cb76f8acc8b62
Ros <pavel.official@bk.ru>
2021-04-05 17:18:31 +0300

    little chenges

--- ConsoleFileManager/ConsoleFileManager/ConsoleFileManager.cs
+++ ConsoleFileManager/ConsoleFileManager/ConsoleFileManager.cs
@@ -2,16 +2,33 @@
 using ConsoleFileManager.Controllers.Commands;
 using ConsoleFileManager.Controls;
 using ConsoleFileManager.View;
+using System;
 
 namespace ConsoleFileManager
 {
 
     public class ConsoleFileManager
     {
+        /// <summary>Перечисление всех команд.</summary>
+        enum Commands
+        {
+            ChangeActivePanel,
+            ChangeDirectoryOrRunProcess,
+            ViewFile,
+            FindFile,
+            Copy,
+            Move,
+            CreateDirectory,
+            Rename,
+            Delete,
+            exit
+        }
+
         public static void Main()
         {
             Controller controller = new Controller();
             ConsoleView view = new ConsoleView();
+
             view.SetCommand(0, new DeleteCommand(controller));
             view.Explore();
         }
